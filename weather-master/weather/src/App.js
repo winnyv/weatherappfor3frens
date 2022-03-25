f29e7d102f2ae3fc2606fcad2804223083ac9ad5
knewbeii <afakmainitis@icloud.com>
2022-03-25 12:47:19 +0000

    comments

--- weather-master/weather/src/App.js
+++ weather-master/weather/src/App.js
@@ -37,42 +37,42 @@ function App() {
         return false
     }
 
-    //state that keeps that setting of large font
+   //the state  keeps the setting of Metric 
     const [largeFont, setLF] = useState(false);
 
-    //state  keeping whether the sidebar should be shown
+   //it keeps that setting of large font
     const [showSidebar, setSidebar] = useState(false);
 
-    //state  keeping whether the search bar should be shown
+    // it decides wther sidebar should be displayed or not
     const [showSearchBar, setSearchBar] = useState(false);
 
-    //state keeping whether the search bar should display the warning (location not found warning)
+    // it decides wether searchbar should be displayed or not
     const[warningSearch,setWarningSearch] = useState(false);
 
-    //state  keeping whether the api respose
+    // it makes sure wether api shows response or not
     const [weatherData, setWeatherData] = useState({});
 
-    //state keeping the city
+   //it makes sure that state  keep the city
     const [city, setCity] = useState("London")
 
-    //state keeping the country
+    // country is assigned to the state
     const [country, setCountry] = useState("GB")
     
     //state to keep track of whether it is the initial render
     const initRender = useRef(true)
 
-    //this is an async method that will call the method that will actually do the API calls.
+    // the async method helps to do the api calls.
     const fetchData = async (query) => {
         return await getJSONWeather(query)
     }
 
     useEffect(() => {
         if (initRender.current) {
-            //fetch data for the initial render
+            // initial render is fetched
             fetchData("London");
             initRender.current = false;
         } else {
-            //inserting the labels into the buttons
+            // labels are inserted into buttons
             var buttons = document.querySelectorAll("[aria-label=\"Go to slide 1\"]");
             var text = document.createTextNode("Home");
             if (!buttons[0].hasChildNodes()) {
@@ -97,13 +97,13 @@ function App() {
         setSidebar(!showSidebar);
     };
 
-    //toggle showSearch and preventDefault on event
+    //the showsearch is toggeled
     const toggleSearch = (event) => {
         event.preventDefault();
         setSearchBar(!showSearchBar)
     }
 
-    //toggle without preventing event
+    //event is togggled without preventing
     const toggleSearchNoArg = () => {
         setSearchBar(!showSearchBar)
     }
@@ -118,7 +118,7 @@ function App() {
             setWarningSearch(true);
         }
     }
-    //when the api response is not ready display loading 
+    // diplay the loading when api response is not ready
     if (initRender.current) {
         return (
             <div id="loading">
